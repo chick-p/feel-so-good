3c22bf6811d1f1392c5566da2ddb9dbaf3a84d6d
chick-p <chick.p.1120@gmail.com>
2019-03-22 20:57:52 +0900

    bin/rails g controller scores index show new edit

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,13 @@
+class ScoresController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'scores/index'
+  get 'scores/show'
+  get 'scores/new'
+  get 'scores/edit'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
