9a79ab8caa0925fdc9ebbcf1ee9a8db4f4b17f7f
chick-p <chick.p.1120@gmail.com>
2019-03-22 20:53:48 +0900

    Added: scheme db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_030549) do
+ActiveRecord::Schema.define(version: 2019_03_22_114551) do
+
+  create_table "scores", force: :cascade do |t|
+    t.date "wakeup_on", null: false
+    t.integer "score", null: false
+    t.string "reason", limit: 50, null: false
+    t.string "cause", limit: 50
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
