4f472bf250bf2c720c6822b924fdaa5ccf59266a
chick-p <chick.p.1120@gmail.com>
2019-03-22 23:20:54 +0900

    Added: create score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -6,8 +6,21 @@ class ScoresController < ApplicationController
   end
 
   def new
+    @score = Score.new
+  end
+
+  def create
+    score = Score.new(score_params)
+    score.save!
+    redirect_to scores_url, notice: "#{score.wakeup_on} の気分を登録しました。"
   end
 
   def edit
   end
+
+  private
+  def score_params
+    params.require(:score).permit(:wakeup_on, :score, :reason, :cause)
+  end
+
 end
