45714df9332124911376beebe51018f1f3a082e3
chick-p <chick.p.1120@gmail.com>
2019-03-23 13:20:39 +0900

    Added: score is related to user

---
+++ db/migrate/20190323034448_add_user_id_to_tasks.rb
@@ -0,0 +1,10 @@
+class AddUserIdToTasks < ActiveRecord::Migration[5.2]
+  def up
+    execute 'DELETE FROM scores;'
+    add_reference :scores, :user, index: true
+    change_column_null :scores, :user_id, false
+  end
+  def down
+    remove_reference :scores, :user, index: true
+  end
+end
