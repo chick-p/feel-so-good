d3a38565bd37baa3783c6dec3a3d9baa346d0370
chick-p <chick.p.1120@gmail.com>
2019-03-23 15:12:58 +0900

    Changed: db scheme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_034448) do
+ActiveRecord::Schema.define(version: 2019_03_23_060338) do
 
   create_table "scores", force: :cascade do |t|
     t.date "wakeup_on", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_03_23_034448) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
