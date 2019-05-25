8941f6f397f9d9e043a2ac9a4eb69b2fc72573ec
chick-p <chick.p.1120@gmail.com>
2019-05-25 20:17:37 +0900

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get '/auth/:provider/callback', to: 'sessions#omniauth'
   delete '/logout', to: 'sessions#destroy'
 
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
   root to: 'scores#index'
 end
